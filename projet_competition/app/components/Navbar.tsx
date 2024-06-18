c86c4312690761a67a5bfe9e2049d29116e8491b
Jules Fakhouri <jules.fakhouri@qobuz.com>
2024-06-18 23:28:16 +0200

    Update and fix Header component layout and styles

--- projet_competition/app/components/Navbar.tsx
+++ projet_competition/app/components/Navbar.tsx
@@ -2,17 +2,15 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const Navbar = () => (
-    <nav>
-        <div className="container mx-auto p-4 flex justify-between items-center">
-            <div className="flex items-center">
+    <nav className=" pt-7 px-6 ">
+        <div className="container   p-4 flex justify-between items-center">
+            <div className="flex   items-center">
                 <Link href="/">
-                    <Image src="/images/logo-poma.svg" alt="Logo Poma" width={96} height={26.17} className="mr-2 cursor-pointer"/>
+                    <Image src="/images/logo-poma.svg" alt="Logo Poma" width={96} height={26} className="mr-2 cursor-pointer"/>
                 </Link>
             </div>
             <Link href="/download">
-                <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700 sm:ml-4">
-                    Télécharger l&apos;app
-                </button>
+                <button className="mt-1  text-[#74549F] font-semibold  border-2 border-[#74549F]  px-2 py-2 rounded-[7.21px]">Télécharger l&apos;app</button>
             </Link>
         </div>
     </nav>
