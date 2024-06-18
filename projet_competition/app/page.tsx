2b430f53edb4695a62b0e5b615824aeba6a9ff6a
Jules Fakhouri <jules.fakhouri@qobuz.com>
2024-06-18 23:33:07 +0200

    add component

--- projet_competition/app/page.tsx
+++ projet_competition/app/page.tsx
@@ -1,13 +1,25 @@
-import Navbar from './components/Navbar';
-import Header from './components/Header';
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 import Solution from "@/app/components/Solution";
+import Footer from "./components/footer";
+import HeroOne from "./components/hero1";
+import AdventurePartnerSection from './components/partner'
+import Aunty from "./components/aunty";
+import Hero2 from './components/hero2'
+
+// overflow-x-hidden
 
 export default function HomePage() {
   return (
-      <div>
-        <Navbar />
-          <Header />
-          <Solution />
-      </div>
+    <div className="bg-purple-100 overflow-x-hidden ">
+      <Navbar />
+      <Header />
+      <Solution />
+      <Aunty/>
+      <AdventurePartnerSection />
+      <HeroOne />
+      <Hero2/>
+      <Footer />
+    </div>
   );
 }
