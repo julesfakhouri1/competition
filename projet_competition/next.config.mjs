be68579c4c6fd875b8ffd7130f0850cf8509d0d8
Jules Fakhouri <jules.fakhouri@qobuz.com>
2024-06-20 00:34:15 +0200

    change configuration

--- projet_competition/next.config.mjs
+++ projet_competition/next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
 
-export default nextConfig;
+export default {
+    webpack(config, { dev, isServer }) {
+      return config;
+    },
+  };
