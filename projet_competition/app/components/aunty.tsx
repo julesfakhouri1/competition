4bfc72ec61457b8dcc026c93547e2f6fbfb63684
Jules Fakhouri <jules.fakhouri@qobuz.com>
2024-06-20 00:49:11 +0200

    fix: escape special characters

--- projet_competition/app/components/aunty.tsx
+++ projet_competition/app/components/aunty.tsx
@@ -1,25 +1,23 @@
 import Image from 'next/image';
 
 const Aunty = () => (
-
-    
     <section className="mt-[10%] px-4 py-16">
-        <div className="absolute top-[2049.85px] left-[90%]
-">
-    <Image src="/images/Vector 12.svg" alt="bar" width={236.14} height={979.35}/>
-    </div>
-        <h2 className="text-6xl max-sm:text-2xl  font-semibold text-center pb-8 text-[#4C3768]">Correspondance par préférence</h2>
+        <div className="absolute top-[2049.85px] left-[90%]">
+            <Image src="/images/Vector 12.svg" alt="bar" width={236.14} height={979.35} />
+        </div>
+        <h2 className="text-6xl max-sm:text-2xl font-semibold text-center pb-8 text-[#4C3768]">Correspondance par préférence</h2>
         <div className="flex flex-col w-[100%] mt-8 md:flex-row justify-between mt-8 max-sm:mx-[5%] mx-[15%]">
-            <div className=" max-sm:w-[90%]">
-                <Image src="/images/V3 4.svg" alt="aunty" width={400} height={400}/>
-
+            <div className="max-sm:w-[90%]">
+                <Image src="/images/V3 4.svg" alt="aunty" width={400} height={400} />
             </div>
-            <div className=" relative max-sm:relative max-sm:left-0 left-6   flex-col   flex  justify-center py-4  rounded-lg  flex-1">
-                
-                <h3 className="mt-4 text-xl max-sm:text-sm text-[#4C3768] my-4 max-sm:w-[100%] w-[80%]">Grâce à notre système de correspondance par QCM, vous êtes associé à des âmes semblables, assurant une alliance harmonieuse.</h3>
-                <p className="mt-2 text-[#4C3768] max-sm:text-sm text-xl max-sm:w-[100%] w-[70%] ">Les affinités communes nourrissent l'identité de chaque petits-enfants de coeur. </p>
+            <div className="relative max-sm:relative max-sm:left-0 left-6 flex-col flex justify-center py-4 rounded-lg flex-1">
+                <h3 className="mt-4 text-xl max-sm:text-sm text-[#4C3768] my-4 max-sm:w-[100%] w-[80%]">
+                    Grâce à notre système de correspondance par QCM, vous êtes associé à des âmes semblables, assurant une alliance harmonieuse.
+                </h3>
+                <p className="mt-2 text-[#4C3768] max-sm:text-sm text-xl max-sm:w-[100%] w-[70%]">
+                    Les affinités communes nourrissent l&apos;identité de chaque petits-enfants de coeur.
+                </p>
             </div>
-            
         </div>
     </section>
 );
--- projet_competition/app/components/hero1.tsx
+++ projet_competition/app/components/hero1.tsx
@@ -6,9 +6,9 @@ const Hero = () => (
     <h2 className="text-2xl font-bold text-center pt-4 text-[#4C3768]">
       S&apos;La quête de Poma
     </h2>
-    <div className="flex flex-wrap  justify-around mt-8 ">
-      <div className="bg-[#DDD4E7] flex    text-center   items-center justify-center p-4 max-sm:p-0 rounded-lg m-2">
-        <div className=" h-[16rem] w-[200px] py-8 ">
+    <div className="flex flex-wrap justify-around mt-8">
+      <div className="bg-[#DDD4E7] flex text-center items-center justify-center p-4 max-sm:p-0 rounded-lg m-2">
+        <div className="h-[16rem] w-[200px] py-8">
           <Image
             src="/images/Group 692.svg"
             alt="ILLUSTRATION"
@@ -17,7 +17,6 @@ const Hero = () => (
           />
         </div>
         <div className="max-sm:w-[50%]">
-          {" "}
           <h3 className="mt-4 max-sm:text-sm w-1/2 max-sm:-ml-[50%] max-sm:w-[200%] text-xl font-bold text-[#4C3768] w-full">
             Rencontre physique
           </h3>
@@ -27,8 +26,8 @@ const Hero = () => (
         </div>
       </div>
 
-      <div className="bg-[#DDD4E7] flex    text-center   items-center justify-center p-4 max-sm:p-0 rounded-lg m-2">
-        <div className=" h-[16rem] w-[200px] py-8 ">
+      <div className="bg-[#DDD4E7] flex text-center items-center justify-center p-4 max-sm:p-0 rounded-lg m-2">
+        <div className="h-[16rem] w-[200px] py-8">
           <Image
             src="/images/Frame (2).svg"
             alt="ILLUSTRATION"
@@ -47,19 +46,18 @@ const Hero = () => (
       </div>
     </div>
 
-    <div className="bg-[#DDD4E7] flex flex-row max-sm:flex-col     text-center   items-center justify-between p-4 max-sm:p-0 rounded-lg my-2 mx-[1%] ">
-      <div className="max-sm:w-[50%]  px-4">
-        {" "}
-        <h3 className="mt-4 max-sm:text-sm w-[50%] flex justify-start text-start max-sm:-ml-[50%] max-sm:w-[200%] text-xl font-bold text-[#4C3768] ">
-          Calendrier d’activité
+    <div className="bg-[#DDD4E7] flex flex-row max-sm:flex-col text-center items-center justify-between p-4 max-sm:p-0 rounded-lg my-2 mx-[1%]">
+      <div className="max-sm:w-[50%] px-4">
+        <h3 className="mt-4 max-sm:text-sm w-[50%] flex justify-start text-start max-sm:-ml-[50%] max-sm:w-[200%] text-xl font-bold text-[#4C3768]">
+          Calendrier d&apos;activité
         </h3>
         <p className="mt-2 text-sm flex w-[40%] max-sm:w-[200%] max-sm:relative max-sm:-left-[50%] text-[20px] max-sm:text-[14px] text-start text-[#4C3768]">
           Avec un calendrier prévu pour réserver les activités, nous offrons à
-          nos membres la possibilité de s'engager dans ces aventures
+          nos membres la possibilité de s&apos;engager dans ces aventures
           collectives.
         </p>
       </div>
-      <div className=" h-[16rem]  w-[70%]  ">
+      <div className="h-[16rem] w-[70%]">
         <Image
           className="max-sm:relative max-sm:top-12"
           src="/images/Frame (3).svg"
@@ -86,9 +84,9 @@ const Hero = () => (
       </div>
     </div>
 
-    <div className="w-[100%] max-sm:-mt-[30%] max-sm: pb-10 self-center text-center center">
+    <div className="w-[100%] max-sm:-mt-[30%] pb-10 self-center text-center center">
       <Link href="/download">
-        <button className="mt-8 max-sm:mt-[40%]   center bg-[#74549F] text-white px-6 py-2 rounded-[7.21px]">
+        <button className="mt-8 max-sm:mt-[40%] center bg-[#74549F] text-white px-6 py-2 rounded-[7.21px]">
           Rejoignez-nous
         </button>
       </Link>
