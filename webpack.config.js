f67da20eb10e48726544f238302d30a503846e61
Jules Fakhouri <jules.fakhouri@qobuz.com>
2024-07-03 22:16:20 +0200

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css", // optional
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              url: false, // optional
+            },
+          },
+        ],
       },
     ],
   },
