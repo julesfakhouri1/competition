b98724b81f316bd5c37bd49572ef0bbd271e371e
Jules Fakhouri <jules.fakhouri@qobuz.com>
2024-07-03 21:17:12 +0200

    create contants

---
+++ projet_competition/constants/index.ts
@@ -0,0 +1,95 @@
+export const profile_one = [
+  {img: '/images/portrait-young-woman-listening-music-through-mobile-phone-stairs 2 (1).svg', name:'Eli',  description: ' 18 ans - Étudiant'},
+  {img: '/images/fashionable-handsome-man-model-posing.png', name:'Hugo' , description: '22 ans - Diplômé'},
+  {img: '/images/portrait-young-woman-listening-music-through-mobile-phone-stairs.png', name:'Louna'  , description: '19 ans - Étudiante'},
+]
+
+export const profile_people = [
+  {img: '/images/stunning-european-girl-lying-grass-laughing-pleasant-young-woman-posing-park-with-cheerful-smile.png', job: 'Mannequin', description: 'Élena, 19 ans - Paris, France'},
+  {img: '/images/smiley-man-taking-selfie-outdoors-medium-shot.png', job: 'Ingénieur en informatique', description: 'Steph, 22 ans - Paris, France'},
+  {img: '/images/portrait-young-woman-listening-music-through-mobile-phone-stairs.png', job: 'Étudiante', description: 'Victorine, 18 ans - Paris, France'},
+  {img: '/images/boy-holding-notebook-outside.png', job: 'Étudiant', description: 'Léo, 18 ans - Paris, France'},
+  {img: '/images/smiley-man-relaxing-outdoors.png', job: 'Comptable', description: 'Victor, 23 ans - Paris, France'},
+  {img: '/images/happy-mature-woman-outdoor.png', job: 'Chef de projet', description: 'Alia, 25 ans - Paris, France'},
+]
+
+export const activityList = [
+  [{img: '', name: 'Sports', placeholder: 'Illustrator'},
+  {img: '', name: 'Art & loisirs créatifs', placeholder: 'Illustrator'},],
+
+  [{img: '', name: 'Musique', placeholder: 'Illustrator'},
+  {img: '', name: 'Cours & formation', placeholder: 'Illustrator'},],
+
+  [{img: '', name: 'Activités plein air', placeholder: 'Illustrator'},
+  {img: '', name: 'Jeux', placeholder: 'Illustrator'},],
+
+  [{img: '', name: 'Bricolage & jardinage', placeholder: 'Illustrator'},
+  {img: '', name: 'Technologie', placeholder: 'Illustrator'},]
+]
+
+export const profile_two = [
+  {img: '/images/fashionable-handsome-man-model-posing.png', name: 'Hugo 22 ans Diplomé'},
+  {img: '/images/portrait-young-woman-listening-music-through-mobile-phone-stairs 2 (1).svg', name: 'Eli 18 ans Étudiant'},
+  {img: '/images/portrait-braless-woman-outdoors-with-camera.png', name: 'Louna 19 ans Étudiante'},
+  {img: '/images/smiling-beautiful-woman-sitting-bench-outdoors.png', name: 'Lea 20 ans Diplomé'},
+]
+
+export const profile_three = [
+  {img: '/images/portrait-young-woman-listening-music-through-mobile-phone-stairs.png', description: 'Victorine, 18 ans - Paris, France', job: 'Étudiant'},
+  {img: '/images/young-beautiful-happy-woman-gray-background.png', description: 'Eléna, 19 ans - Paris, France', job: 'Mannequin'},
+  {img: '/images/asian-model-wearing-plain-white-tshirt-tshirt-mockup-your-designasian-model-wearing-plain-whi.png', description: 'Simon, 22 ans - Paris, France', job: 'Artiste'},
+  {img: '/images/boy-holding-notebook-outside.png', description: 'Léo, 18 ans - Paris, France', job: 'Étudiant'},
+  {img: '/images/smiley-man-relaxing-outdoors.png', description: 'Victor, 23 ans - Paris, France', job: 'Comptable'},
+  {img: '/images/happy-mature-woman-outdoor.png', description: 'Alia, 25 ans - Paris, France', job: 'Chef de projet'},
+]
+
+export const profile_four = [
+  {img: '/images/angle-eleve-nageur-masculin-dans-piscine-eau.png', name: 'Natation', date: '12 - 18 Juin ', location: 'Paris' },
+  {img: '/images/young-girl-with-trekking-poles-walking-autumn-mountain-trail.png', name: 'Randonné', date: '14 - 19 Juin ', location: 'Nice' },
+  {img: '/images/high-angle-scouts-playing-with-map.png', name: 'Défi orientation', date: '22 - 24 Juin', location: 'Ardèche' },
+  {img: '/images/V3.png', name: 'Cours de cuisine', date: '26 Juin', location: 'Lyon' },
+]
+
+export const filter_profile_four = [
+  [{img: '/images/angle-eleve-nageur-masculin-dans-piscine-eau.png', name: 'Natation', date: '12 - 18 Juin ', location: 'Paris' },
+  {img: '/images/young-girl-with-trekking-poles-walking-autumn-mountain-trail.png', name: 'Randonné', date: '14 - 19 Juin ', location: 'Nice' }],
+  [{img: '/images/high-angle-scouts-playing-with-map.png', name: 'Défi orientation', date: '22 - 24 Juin', location: 'Ardèche' },
+  {img: '/images/V3.png', name: 'Cours de cuisine', date: '26 Juin', location: 'Lyon' }],
+]
+
+
+export const notification_date = [
+  {day: 'L', number: '24'},
+  {day: 'M', number: '25'},
+  {day: 'M', number: '26'},
+  {day: 'J', number: '27'},
+  {day: 'V', number: '28'},
+  {day: 'S', number: '29'},
+  {day: 'D', number: '30'},
+]
+
+export const left_notification_info = [
+  {bold: '11:35', light: '11:35'},
+  {bold: '15:15', light: '16:30'},
+  {bold: '18:00', light: '20:00'},
+  {bold: '19:35', light: '21:35'},
+]
+
+export const notification_detial = [
+  {activity: 'Cours de cuisine', loaction: 'Paris', user: 'Cours de cuisine', date: '18 ans', img: '/images/young-girl-with-trekking-poles-walking-autumn-mountain-trail.png' },
+  {activity: 'Cours de cuisine', loaction: 'Paris', user: 'Avec Léa Poitier', date: '19 ans', img: '/images/angle-eleve-nageur-masculin-dans-piscine-eau.png' },
+  {activity: 'Randonné', loaction: 'Paris', user: 'Avec Antony Robert', date: '20 ans', img: '/images/high-angle-scouts-playing-with-map.png', name: 'Défi orientation' },
+  {activity: 'Cours de cuisine', loaction: 'Italy', user: 'Avec Antony Robert', date: '21 ans', img: '/images/V3.png' },
+]
+
+
+
+export const interest_one = [  'Lecture', 'Peinture/Dessin', 'Jardinage', 'Cuisine', 'Cuisine' ]
+
+export const interest_two = [ 'Rencontrer de nouvelles personnes', 'Partager des expériences & histoires', 'Offrir un soutient et de la compagnie', 'Participer à des activités de groupe', 'Autre ( veuillez préciser )' ]
+
+export const interest_three = [ 'Histoire de vie', 'Compétences professionnelles', 'Hobbies et intérêt', 'Expérience et voyage', 'Autre ( veuillez préciser )']
+
+export const interest_four = ['Quotidiennement', 'Plusieurs fois par semaine', 'Une fois par semaine', 'Plusieurs fois par mois', 'Une fois par mois', 'Moins souvent']
+
+export const interest_five = ['Messagerie texte', 'Appels téléphone', 'Appel vidéo', 'Emails', 'Réseaux sociaux', 'Autres ( veuillez préciser )']
